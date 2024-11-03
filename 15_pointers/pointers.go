252781b2b376c779b046271f23d89527ce417172
NAHID12105080 <nahid151341@gmail.com>
2024-11-03 09:20:45 +0600

    pointers included

---
+++ 15_pointers/pointers.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+//by default variables are passed by value
+//if you want to pass by reference you can use pointers
+//pointers are the memory address of the variable
+
+
+
+func chageNumber(number *int){
+	*number = 20
+}
+
+
+func main() {
+ number:= 10
+ chageNumber(&number)
+ fmt.Println(number)
+}
